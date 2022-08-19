341552928d02d65026982dac9a25e4db4e3a1de0
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-19 11:53:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+//matching characters
+function findMatching (driverList, name) {
+    return driverList.filter(function (driverName) {
+      return driverName.toLowerCase() === name.toLowerCase();
+    });
+  }
+  
+  //matching letters
+  function fuzzyMatch(driverList , providedName){
+    let lengthOfName = providedName.length
+    return driverList.filter(function(driverName){
+        return driverName.slice(0,lengthOfName) === providedName;//used slice to return the part with the first index
+
+    });
+
+  }
+  function matchName(driverList, name){
+    return driverList.filter(function(driverInfo){
+        return driverInfo.name.toLowerCase() === name.toLowerCase();
+
+    })
+
+  }
+
+
+ 
+  
+  /*--function matchName (driverList, name) {
+    return driverList.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
+  --*/
